58db81f840262df3c86dced2c6541291d6b32f90
Marcelo.Guedes <guedes.marcelocarvalho@gmail.com>
2025-01-31 19:36:47 -0300

    Salvando mudanças antes de reverter

--- src/main/java/br/com/petz/clientepet/cliente/Cliente.java
+++ src/main/java/br/com/petz/clientepet/cliente/Cliente.java
@@ -1,4 +1,41 @@
 package br.com.petz.clientepet.cliente;
 
 public class Cliente {
+<<<<<<< Updated upstream
+=======
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
+    private UUID idCliente;
+    @NotBlank
+    private String nomeCompleto;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String celular;
+    private String telefone;
+    private Sexo sexo;
+    @NotNull
+    private LocalDate dataNascimento;
+    @CPF
+    private String cpf;
+    @NotNull
+    private Boolean aceitaTermos;
+
+    private LocalDateTime dataHoraDoCadastro;
+    private LocalDateTime dataHoraDaUltimaAlteracao;
+
+    private Cliente(String nomeCompleto, String email, String celular, String telefone, Sexo sexo, LocalDate dataNascimento, String cpf, Boolean aceitaTermos) {
+        this.nomeCompleto = nomeCompleto;
+        this.email = email;
+        this.celular = celular;
+        this.telefone = telefone;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+        this.aceitaTermos = aceitaTermos;
+        this.dataHoraDoCadastro = LocalDateTime.now();
+    }
+>>>>>>> Stashed changes
 }
