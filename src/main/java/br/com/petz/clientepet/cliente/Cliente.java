69a7548055c14f742cc9631d3cc31eab4e3a12c9
Marcelo.Guedes <guedes.marcelocarvalho@gmail.com>
2025-01-31 18:52:22 -0300

    ajusta mapeamento id uuid

--- src/main/java/br/com/petz/clientepet/cliente/Cliente.java
+++ src/main/java/br/com/petz/clientepet/cliente/Cliente.java
@@ -1,4 +1,54 @@
 package br.com.petz.clientepet.cliente;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.UUID;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
 public class Cliente {
+    @Id@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
+    private UUID idCliente;
+    @NotBlank
+    private String nomeCompleto;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String celular;
+    private String telefone;
+    private Sexo sexo;
+    @NotNull
+    private LocalDate dataNascimento;
+    @CPF
+    private String cpf;
+    @NotNull
+    private Boolean aceitaTermos;
+
+    private LocalDateTime dataHoraDoCadastro;
+    private LocalDateTime dataHoraDaUltimaAlteracao;
+
+    private Cliente(String nomeCompleto, String email, String celular, String telefone, Sexo sexo, LocalDate dataNascimento, String cpf, Boolean aceitaTermos) {
+        this.nomeCompleto = nomeCompleto;
+        this.email = email;
+        this.celular = celular;
+        this.telefone = telefone;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+        this.aceitaTermos = aceitaTermos;
+        this.dataHoraDoCadastro = LocalDateTime.now();
+    }
 }
--- src/main/java/br/com/petz/clientepet/cliente/Sexo.java
+++ src/main/java/br/com/petz/clientepet/cliente/Sexo.java
@@ -1,4 +1,5 @@
 package br.com.petz.clientepet.cliente;
 
 public enum Sexo {
+    FEMININO,MASCULINO;
 }
