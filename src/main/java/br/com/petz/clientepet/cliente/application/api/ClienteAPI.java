2dc6d46f11ca33d631e1bb5442606862585c2005
Marcelo.Guedes <guedes.marcelocarvalho@gmail.com>
2025-02-04 20:48:35 -0300

    implementa controller

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.*;
 public interface ClienteAPI {
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    ClienteResponse postCliente(@RequestBody ClienteRequest clienteRequest)
+    ClienteResponse postCliente (@RequestBody ClienteRequest clienteRequest);
 }
--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -1,4 +1,16 @@
 package br.com.petz.clientepet.cliente.application.api;
 
-public class ClienteController {
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+public class ClienteController  implements  ClienteAPI {
+
+    @Override
+    public ClienteResponse postCliente(ClienteRequest clienteRequest){
+        log.info("[inicia] ClienteController - postCliente");
+        log.info("[finaliza] ClienteController - postCliente");
+        return null;
+    }
 }
